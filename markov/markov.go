609575a9a382228e345d45959d711bc7302ef073
Daniel Roth <daniel.roth02@sap.com>
2018-01-09 08:25:22 -0800

    small adjustments

--- markov/markov.go
+++ markov/markov.go
@@ -11,37 +11,7 @@ import (
 	"strings"
 )
 
-// train: function(text, factor = 1) {
-// 	var dict = {};
-
-// 	var words = text.split(/\s/).filter(function(word){ // get all words in text.
-// 		return word.length > 0;
-// 	});
-
-// 	for (var i = 0; i < words.length; i++) {
-// 		// words[i] = words[i].toLowerCase();
-// 		// words[i] = words[i].replace(/[^-a-z0-9\']/i, ''); // remove also punctuation
-// 		words[i].trim();
-// 	}
-
-// 	for (var i = 0; i < words.length - 1; i++) {
-// 		var a = words[i];
-// 		var b = words[i + 1];
-// 		if (typeof dict[a] == 'undefined') {
-// 			dict[a] = {};
-// 		}
-// 		if (typeof dict[a][b] == 'undefined') {
-// 			dict[a][b] = factor;
-// 		} else {
-// 			dict[a][b] = dict[a][b] + dict[a][b] * factor;
-// 		}
-// 	}
-
-// 	// http://stackoverflow.com/questions/171251/how-can-i-merge-properties-of-two-javascript-objects-dynamically
-// 	//Object.assign(dictionary, dict);
-
-// 	return dict;
-// },
+// Train from string of words
 func Train(text string, factor int) map[string]map[string]int {
 	dict := make(map[string]map[string]int)
 
@@ -61,31 +31,13 @@ func Train(text string, factor int) map[string]map[string]int {
 	return dict
 }
 
-// trainFromFile: function(path) {
-// 	var text = fs.readFileSync(path, 'utf8');
-// 	return this.train(text);
-// },
+// TrainFromFile takes a file path, reads the file and passes the string to Train
 func TrainFromFile(path string, factor int) map[string]map[string]int {
 	buf, _ := ioutil.ReadFile(path)
 	return Train(string(buf), factor)
 }
 
-// trainFromFolder: function(path) {
-// 	var files = fs.readdirSync(path);
-// 	var txtFiles = files.filter(function(txtPath) {
-// 		return txtPath.match(/.*\.txt$/)
-// 	});
-// 	var dict = [];
-
-// 	for (var i = 0; i < txtFiles.length; i++) {
-// 		var tmp = this.trainFromFile(path + "/" + txtFiles[i]);
-// 		dict = mergeDict(dict, tmp);
-// 	}
-
-// 	console.log("Successfully trained on " + txtFiles.length + " files.");
-
-// 	return dict;
-// },
+// TrainFromFolder takes a path, and passes every text file it finds to TrainFromFile
 func TrainFromFolder(path string, factor int) map[string]map[string]int {
 	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
 	if err != nil {
@@ -103,7 +55,7 @@ func TrainFromFolder(path string, factor int) map[string]map[string]int {
 		match, _ := regexp.MatchString(".*\\.txt$", file.Name())
 		if match {
 			fmt.Println(file.Name())
-			fmt.Println(path + string(os.PathSeparator) + file.Name())
+			fmt.Println(dir + string(os.PathSeparator) + path + string(os.PathSeparator) + file.Name())
 			tmp := TrainFromFile(path+string(os.PathSeparator)+file.Name(), factor)
 			dict = mergeDict(dict, tmp)
 		}
@@ -112,43 +64,7 @@ func TrainFromFolder(path string, factor int) map[string]map[string]int {
 	return dict
 }
 
-// generate: function(dict, maxLength = 0, startWord = '') {
-// 	var word;
-// 	if (startWord == '') {
-// 		word = pickRandom(dict);
-// 	} else {
-// 		word = startWord;
-// 	}
-
-// 	sentence = word;
-
-// 	var i = 0;
-// 	while (maxLength == 0 || i < maxLength - 1) {
-// 		i++;
-
-// 		var highest = 0;
-// 		var tmp = word;
-// 		for (var val in dict[word]) {
-// 			if (dict[word].hasOwnProperty(val)) {
-// 				// var items = Object.keys(dict[word]).map(function(key) {
-// 				//     return [key, dict[word][key]];
-// 				// });
-// 				// items.sort(function(first, second) {
-// 				//     return second[1] - first[1];
-// 				// });
-// 				// word = items[0][0];)
-// 				word = pickRandom(dict[word]);
-// 			}
-// 		}
-
-// 		if (typeof word == 'undefined' || word == tmp) {
-// 			return sentence;
-// 		}
-// 		tmp = word;
-// 		sentence = sentence + ' ' + word;
-// 	}
-// 	return sentence;
-// },
+// Generate takes a dictionary, a maximum length and a startword to generate a text based on the inputs
 func Generate(dict map[string]map[string]int, maxLength int, startWord string) string {
 	var word = ""
 	if startWord == "" {
@@ -210,23 +126,7 @@ func Generate(dict map[string]map[string]int, maxLength int, startWord string) s
 // 	return dict;
 // }
 
-// function mergeDict(dict1, dict2) {
-//     var dict3 = dict1;
-//     for (var val in dict2) {
-//         if (typeof dict3[val] == 'undefined') {
-//             dict3[val] = dict2[val];
-//         } else {
-//             for (var sval in dict2[val]) {
-//                 if (typeof dict3[val][sval] == 'undefined') {
-//                     dict3[val][sval] = dict2[val][sval];
-//                 } else {
-//                     dict3[val][sval] = dict3[val][sval] + dict2[val][sval];
-//                 }
-//             }
-//         }
-//     }
-//     return dict3;
-// }
+// mergeDict, given two dictionaries merges them into one
 func mergeDict(dict1, dict2 map[string]map[string]int) map[string]map[string]int {
 	res := dict1
 	for k := range dict2 {
@@ -246,16 +146,7 @@ func mergeDict(dict1, dict2 map[string]map[string]int) map[string]map[string]int
 	return res
 }
 
-// function pickRandom(dict) {
-//     var keys = [];
-//     for (var val in dict) {
-//         if (dict.hasOwnProperty(val)) {
-//             keys.push(val);
-//         }
-//     }
-
-//     return keys[Math.floor(Math.random() * keys.length)];
-// }
+// pickRandom takes a dictionary and selects a random key
 func pickRandom(dict map[string]map[string]int) string {
 	keys := make([]string, len(dict))
 	i := 0
@@ -266,6 +157,7 @@ func pickRandom(dict map[string]map[string]int) string {
 	return keys[rand.Intn(len(keys))]
 }
 
+// pickRandomFollwup takes a dictionary and selects a random key
 // because of type specific parameters, same content as above, different paremter...
 // IS THIS NECESSARY?????
 func pickRandomFollwup(dict map[string]int) string {
